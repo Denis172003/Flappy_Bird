25c03bfad9681b5951f79ab516c26f437559223c
Denis172003 <denis.mitica@gmail.com>
2023-10-31 01:16:14 +0200

    commit #1

--- COLLISION/COLLISION.h
+++ COLLISION/COLLISION.h
@@ -1,18 +1,1 @@
-#include <SFML/Graphics.hpp>
-#ifndef OOP_COLLISION_H
-#define OOP_COLLISION_H
 
-
-class Collision
-{
-public:
-    Collision();
-    ~Collision();
-
-    bool CheckSpriteCollision(sf::Sprite sprite1, sf::Sprite sprite2);
-    bool CheckSpriteCollision(sf::Sprite sprite1, float scale1, sf::Sprite sprite2, float scale2);
-
-};
-
-
-#endif //OOP_COLLISION_H
